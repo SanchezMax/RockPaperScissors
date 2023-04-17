eabf9b26f2e1f8275fa01ebd046b724ab6aef5ed
Max Zykin <sanchez.max@yandex.ru>
2023-04-17 16:30:12 +0400

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Aleksey Novikov on 17.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
