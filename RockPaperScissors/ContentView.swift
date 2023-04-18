34c067679cd278c0ce21ae25e215ababaff3e2a1
Max Zykin <sanchez.max@yandex.ru>
2023-04-18 10:28:52 +0400

    end

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,14 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentChoise = "rock"
+    @State private var shouldWin = Bool.random()
+    @State private var alertShow = false
+    
+    @State private var score = 0
+    @State private var questionsLeft = 10
+    
+    let possibleChoises = ["rock", "paper", "scissors"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("Current score: \(score)")
+            Text("App's move: \(currentChoise.capitalized)")
+            Text("You should \(shouldWin ? "win" : "lose")")
+            HStack {
+                ForEach(possibleChoises, id: \.self) { choise in
+                    Button(choise.capitalized) {
+                        switch shouldWin {
+                        case true:
+                            if possibleChoises.firstIndex(of: currentChoise)! + 1 == possibleChoises.firstIndex(of: choise) || (currentChoise == possibleChoises.last && choise == possibleChoises.first) {
+                                score += 1
+                            } else {
+                                score -= 1
+                            }
+                        case false:
+                            if possibleChoises.firstIndex(of: currentChoise)! - 1 == possibleChoises.firstIndex(of: choise) || (currentChoise == possibleChoises.first && choise == possibleChoises.last) {
+                                score += 1
+                            } else {
+                                score -= 1
+                            }
+                        }
+                        prepareGame()
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+            }
         }
         .padding()
+        .alert("You've finished the game", isPresented: $alertShow) {
+            Button("Ok") {
+                score = 0
+            }
+        } message: {
+            Text("Your final score is \(score)")
+        }
+
+    }
+    
+    func prepareGame() {
+        questionsLeft -= 1
+        currentChoise = possibleChoises[Int.random(in: 0...2)]
+        shouldWin.toggle()
+        if questionsLeft == 0 {
+            alertShow = true
+            questionsLeft = 10
+        }
     }
 }
 
